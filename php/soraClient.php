d149f0336b2ba1f79eeefe6a3fe757b74eeae621
Rirei Kuroshima <rireikuroshima@icloud.com>
2015-10-12 22:50:02 +0900

    php: 実行時の引数で年とクールが指定されていたらその情報を取得する
    第1引数で年、第2引数でクールを指定しないといけない。
    
    Signed-off-by: Rirei Kuroshima <rireikuroshima@icloud.com>

--- php/soraClient.php
+++ php/soraClient.php
@@ -9,7 +9,15 @@ define("BASEURL", "http://api.moemoe.tokyo/");
 define("SUBURL", "anime/v1/master/");
 
 /* URL of ShangriLa Anime API Server (Sora). */
-$url = BASEURL . SUBURL . "cours";
+$url = BASEURL . SUBURL;
+if(array_key_exists(1, $argv)) {
+        $url .= $argv[1];
+        if(array_key_exists(2, $argv)) {
+                $url .= "/" . $argv[2];
+        }
+} else {
+        $url .= "cours";
+}
 
 /* Decodes a JSON string. */
 $json = file_get_contents($url);
