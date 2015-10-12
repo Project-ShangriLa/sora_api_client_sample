7150bc79e690035556f730100185463ad8521a44
Rirei Kuroshima <rireikuroshima@icloud.com>
2015-10-12 23:49:08 +0900

    php: オプションの引数が整数値かどうかチェックする
    is_int()関数を使ってオプションで指定した値が整数値かどうかチェック
    する。もし、整数値じゃなかったら、usage()関数を使って、usageを
    表示する。
    
    また、テストしやすくするために、終了時にexit()関数を使う。
    
    Signed-off-by: Rirei Kuroshima <rireikuroshima@icloud.com>

--- php/soraClient.php
+++ php/soraClient.php
@@ -4,6 +4,30 @@
  * Copyright (c) Rirei Kuroshima
  */
 
+function usage()
+{
+        $usage_string = <<<EOM
+Name
+    soraClient.php - ShangriLa APIを叩くPHPプログラム
+
+Synopsis
+    soraClient.php
+    soraClient.php -y year
+    soraClient.php -y year -n number
+
+Description
+    ShangriLa APIを叩くPHPプログラムです。オプション無しで実行すると、アニメ
+    情報のクールごとの情報のリストを取得します。
+
+    -yを指定すると、yearで指定したYYYY年のアニメ1クールから4クールまでの情報を
+    全て取得します。
+
+    -yと-nオプションを指定すると、yearで指定されたYYYY年アニメのnumberで指定
+    されたクール情報を全て取得します。
+EOM;
+        echo $usage_string . "\n";
+}
+
 $options = getopt("y:n:");
 
 /* Defines a named constant */
@@ -13,8 +37,17 @@ define("SUBURL", "anime/v1/master/");
 /* URL of ShangriLa Anime API Server (Sora). */
 $url = BASEURL . SUBURL;
 if(array_key_exists("y", $options)) {
+        if(is_int($options["y"]) === false) {
+                usage();
+                exit(1);
+        }
         $url .= $options["y"];
+
         if(array_key_exists("n", $options)) {
+                if(is_int($options["n"]) === false) {
+                        usage();
+                        exit(1);
+                }
                 $url .= "/" . $options["n"];
         }
 } else {
@@ -25,5 +58,6 @@ if(array_key_exists("y", $options)) {
 $json = file_get_contents($url);
 
 print($json);
+exit(0);
 
 ?>
