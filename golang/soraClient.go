a38ae1ecfeee4a9518b6706dbd6c613ebe3feaa3
Rirei Kuroshima <rireikuroshima@icloud.com>
2015-10-20 19:20:55 +0900

    golang: 冗長な処理を修正する
    ただ、現状だと./soraClient -n 4と実行すると、./soraClientと同じ
    結果が得られるので気持ち悪い。-yオプションがないとエラーメッセージ
    を出すようにしたい。
    
    Signed-off-by: Rirei Kuroshima <rireikuroshima@icloud.com>

--- golang/soraClient.go
+++ golang/soraClient.go
@@ -22,23 +22,21 @@ func main() {
 	var n = flag.Int("n", 999, "アニメのクール")
 	flag.Parse()
 
+	url = baseurl + suburl
 	if *y != 999 {
 		year, _ = strconv.Atoi(time.Now().Format("2006"))
 		if *y > year {
 			fmt.Println("年を正しく入力してください")
 			return
 		}
+		url += strconv.Itoa(*y)
 
 		if *n != 999 {
 			if *n < 1 || 5 < *n {
 				fmt.Println("クールを正しく入力してください")
 				return
 			}
-
-			url = baseurl + suburl +
-				strconv.Itoa(*y) + "/" + strconv.Itoa(*n)
-		} else {
-			url = baseurl + suburl + strconv.Itoa(*y)
+			url += "/" + strconv.Itoa(*n)
 		}
 	} else {
 		url = baseurl + suburl + "cours"
