fe4569cc55f46c79a2f3f5855701e2ecc81534fd
Issei Fujimoto <fujimoto@fate.bcl.yamaguchi-u.ac.jp>
2015-10-21 19:37:12 +0900

    オプション解析追加

--- python/client.py
+++ python/client.py
@@ -1,47 +1,36 @@
 # -*- coding: utf-8 -*-
+import argparse
 import requests
 import sys
-
-def usage(flag):
-    print (".client.py -y [year] -n [number]")
-    if flag == 1 :
-        print ("renge of [year] is 2013 form 2015.")
-    if flag == 2 :
-        print ("renge of [number] is 1 form 4.")
-    exit(1)
-
 
 if __name__ == '__main__':
     url='http://api.moemoe.tokyo/anime/v1/master/'
     param = sys.argv
     argc = len(param)
-    flag=0
 
-    options = [option for option in param if option.startswith('-')]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-y',type=int,choices=range(2013,2016),default=0)
+    parser.add_argument('-n',type=int,choices=range(1,5),default=0)
+    args=parser.parse_args()
 
     if argc != 1 and argc != 3 and argc != 5 :
-        usage(0)
+        print ("arguments num is wrong.")
+        exit(1)
 
-    if '-h' in options or '--help' in options:
-        usage()
-    if '-y' in options :
-        year = param[2]
-        #if year < "2014" or year > "2015" : usage(1)
-        get = year
-        if '-n' in options :
-            number = param[4]
-            if number < "1" or number > "4" : usage(2)
-            get =   year +"/" + number
-
-    if argc == 1:
-        get = "cours"
+    if args.y == 0:
+        if args.n != 0 :
+            print ("use [-n] with [-y].")
+            exit(1)
+        else :
+            get = "cours"
+    else :
+        get = str(args.y)
+        if args.n != 0 :
+            get = get + "/" +str(args.n)
 
     url = url + get
-
-    r = requests.get(url)
-    print (r.text)
+    print url
+    #r = requests.get(url)
+    #print (r.text)
     exit(0)
-
-
-    #print r.encoding
 
